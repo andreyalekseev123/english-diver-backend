bc411f80ef49cd3094b77ae2add1473961b36392
Andrey Alekseev <andrey.alekseev@saritasa.com>
2021-10-24 17:18:23 +0700

    Remove not ui swagger

--- config/urls.py
+++ config/urls.py
@@ -29,11 +29,6 @@ urlpatterns = [
     
     # swagger paths
     path(
-        "swagger(?P<format>.json|.yaml)",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui"
