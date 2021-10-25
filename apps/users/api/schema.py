4522aeb9c9e23c0616ab5979eba275714160bd4c
andrey <aletor2001@mail.ru>
2021-10-25 09:29:28 +0700

    Fix schema

--- apps/users/api/schema.py
+++ apps/users/api/schema.py
@@ -2,7 +2,11 @@ from rest_framework import status
 
 from apps.core.utils import define_swagger_extend_schema
 
-from .serializers import PasswordResetConfirmSerializer, UserSerializer
+from .serializers import (
+    PasswordResetConfirmSerializer,
+    RegistrationSerializer,
+    UserSerializer,
+)
 from .views import (
     PasswordResetConfirmView,
     PasswordResetView,
@@ -12,13 +16,15 @@ from .views import (
 define_swagger_extend_schema(
     view=RegistrationAPIView,
     method='post',
+    request_body=RegistrationSerializer,
     responses={status.HTTP_201_CREATED: UserSerializer}
 )
 
 define_swagger_extend_schema(
     view=PasswordResetView,
     method='post',
-    responses=None
+    responses=None,
+    request_body=UserSerializer,
 )
 define_swagger_extend_schema(
     view=PasswordResetConfirmView,
